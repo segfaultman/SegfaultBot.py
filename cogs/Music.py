c605b0bb8962ef27ecdf3a5e3be095492f1e83d8
SegfaultMan <yasen_v@outlook.com>
2025-04-18 21:44:12 +0300

    Add some very basic player functionality

--- cogs/Music.py
+++ cogs/Music.py
@@ -12,20 +12,26 @@ class Music(commands.Cog):
         self.bot = bot
         self.pomice = pomice.NodePool()
 
-    async def start_nodes(self) -> None:
+    async def start_main_node(self) -> None:
         await self.pomice.create_node(bot=self.bot, host=os.getenv('LAVALINK_HOST'),
                                       port=int(os.getenv('LAVALINK_PORT')),
                                       password=os.getenv('LAVALINK_PASS'), identifier='MAIN')
         print('Created MAIN Lavalink node')
 
     @override
+    async def cog_load(self) -> None:
+        await self.start_main_node()
+    
+    @override
     async def cog_unload(self) -> None:
         identifiers = list(self.pomice.nodes.keys())
-        print(identifiers)
         for id in identifiers:
             print('Disconnecting node: {}'.format(id))
             await self.pomice.nodes[id].disconnect() # Ensure that we disconnect all nodes on cog reload
-        # Further features like saving the state of players may use that as well
+        # Future features like saving the state of players may use that as well
+
+    async def get_player(self, ctx: commands.Context) -> pomice.Player:
+        pass
 
     @commands.command(name='connect')
     async def connect(self, ctx: commands.Context):
@@ -39,6 +45,104 @@ class Music(commands.Cog):
         await ctx.author.voice.channel.connect(cls=pomice.Player)
         await ctx.send('Connected to: {}'.format(channel))
 
+    @commands.command(name='disconnect')
+    async def disconnect(self, ctx: commands.Context):
+        """
+        Disconnect the bot from the current channel
+        """
+
+        if not ctx.voice_client:
+            return await ctx.send('Currently not connected to any VC')
+        
+        await ctx.voice_client.disconnect()
+        await ctx.send('Disconnected')
+
+    @commands.command(name='play', aliases=['p'])
+    async def play(self, ctx: commands.Context, *, song: str = None):
+        """
+        Searches and plays the specified song
+        """
+
+        if not ctx.voice_client:
+            await ctx.invoke(self.connect)
+
+        if not song:
+            return await ctx.send('Command usage: `?play [song title]`')
+
+        player = ctx.voice_client
+
+        results = await player.get_tracks(query='{}'.format(song), search_type=pomice.enums.SearchType.scsearch)
+
+        if not results:
+            return await ctx.send('Could not find any song matching that title')
+        
+        if isinstance(results, pomice.Playlist):
+            track: pomice.objects.Track = results.tracks[0]
+        else:
+            track: pomice.objects.Track = results[0]
+
+        await player.play(track)
+        await ctx.send('Now playing: {}'.format(track))
+
+    @commands.command(name='stop')
+    async def stop(self, ctx: commands.Context):
+        """
+        Stops the current song
+        """
+
+        if not ctx.voice_client:
+            return await ctx.send('Not even connected to a VC lmao')
+        
+        player = ctx.voice_client
+
+        if not player.is_playing:
+            return await ctx.send('There is nothing playing at the moment')
+        
+        await player.stop()
+        await ctx.send('Stopped the player')
+
+    @commands.command(name='volume', aliases=['v'])
+    async def volume(self, ctx: commands.Context, volume: int):
+        """
+        Sets the volume of the player to the specified value from 0 to 500
+        """
+
+        if not ctx.voice_client:
+            return await ctx.send('Not even connected to a VC lmao')
+        
+        player = ctx.voice_client
+
+        await player.set_volume(volume=volume)
+        await ctx.send('Volume set to {}%'.format(volume))
+
+    @commands.command(name='pause')
+    async def pause(self, ctx: commands.Context):
+        """
+        Pauses the bot player
+        """
+
+        if not ctx.voice_client:
+            return await ctx.send('Not even connected to a VC lmao')
+        
+        player = ctx.voice_client
+
+        await player.set_pause(pause=True)
+        await ctx.send('The player is paused')
+
+    @commands.command(name='resume')
+    async def resume(self, ctx: commands.Context):
+        """
+        Resumes the bot player
+        """
+
+        if not ctx.voice_client:
+            return await ctx.send('Not even connected to a VC lmao')
+        
+        player = ctx.voice_client
+
+        await player.set_pause(pause=False)
+        await ctx.send('The player is resumed')
+
+
 async def setup(bot):
     await bot.add_cog(Music(bot))
-    await bot.cogs['Music'].start_nodes()
